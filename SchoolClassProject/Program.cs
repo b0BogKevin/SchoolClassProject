912da716b72cb0a6559f0f0aaa9ba5f5c7d63abe
BogKevin <b0bogkev@vasvari.org>
2024-05-28 16:31:41 +0200

    enyhe változtatás

--- SchoolClassProject/Program.cs
+++ SchoolClassProject/Program.cs
@@ -1,10 +1,12 @@
 ﻿using SchoolClassProject.Model;
 using SchoolClassProject.Repo;
 
-SchoolClass schoolClass = new SchoolClass(9,'a',9000,2);
+SchoolClass schoolClass = new SchoolClass(9,'a',7000,2);
 SchoolClass schoolClass2 = new SchoolClass(9, 'b', 6000,4);
 
 Console.WriteLine(schoolClass);
+Console.WriteLine(schoolClass2);
+
 Console.WriteLine(schoolClass.HigherFee(schoolClass2));
 
 SchoolClassRepo repo = new();
